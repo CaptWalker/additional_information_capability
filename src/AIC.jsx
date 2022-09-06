207ba1b409c2ed39ec67c8169b39d1d783546629
CaptWalker <28630298+CaptWalker@user.noreply.github.com>
2022-09-06 19:06:13 +0530

    first commit

---
+++ src/AIC.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AIC() {
+  return (
+    <h2>Additional Information Capability</h2>
+  )
+}
+
+export default AIC;
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import AIC from "./AIC";
+
+import "./index.css";
+
+const App = () => (
+  <div>
+    <AIC />
+  </div>
+);
+ReactDOM.render(<App />, document.getElementById("app"));
